ca845844b6e5c30982972a791a3625e3e84fdde1
Sven Dekker <dekker1990@hotmail.com>
2020-01-20 14:17:24 +0100

    Added new exercises

---
+++ currencyConversion.js
@@ -0,0 +1,13 @@
+// Write a program that converts currency. Specifically, convert
+// euros to U.S. dollars. Prompt for the amount of money in
+// euros you have, and prompt for the current exchange rate
+// of the euro. Print out the new amount in U.S. dollars. The
+// formula for currency conversion is
+
+let amountFrom = prompt('How many euros are you exchanging?') 
+let rateFrom = prompt('What is the exchange rate? (€)')
+let rateTo = prompt('What is the exchange rate? ($) ')
+let amountTo =  ((amountFrom * rateFrom) / rateTo).toPrecision(5)
+
+prompt(`${amountFrom} euros at an exchange rate of ${rateFrom} is ${amountTo} U.S. dollars`)
+
---
+++ simpleInterest.js
@@ -0,0 +1,10 @@
+// Create a program that computes simple interest. Prompt for
+// the principal amount, the rate as a percentage, and the time,
+// and display the amount accrued (principal + interest).
+
+let principal = prompt('Enter the principal: ')
+let rate = prompt('Enter the rate of interest: ')
+let years = prompt('Enter the number of years: ')
+let amountAccrued = principal(1 + rate * years)
+
+prompt(`After ${years} years at ${rate}, the investment will be worth $${amountAccrued}`)
